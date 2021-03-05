b18ed3b5a8402b620a1c11d3315a9345f4074d7a
Jelu <67263663+AJelu@users.noreply.github.com>
2021-03-05 13:32:03 +0200

    Delete Camera.cpp

--- GameHalloween/Camera.cpp
+++
@@ -1,9 +0,0 @@
-#include "Camera.h"
-
-View CameraControl::getCharacterCoordinates(float x, float y)
-{
-	camera.setCenter(x + 100, y); //следим за игроком передавая его координаты
-	return camera;
-}
-
-View CameraControl::getCamera() { return camera; }
